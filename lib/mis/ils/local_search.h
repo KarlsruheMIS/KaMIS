f84d056acd26e9bec2079a798515f7126bc21b68
Christian Schulz <christian.schulz@univie.ac.at>
2020-05-27 12:16:23 +0200

    removed GPL from h files

--- lib/mis/ils/local_search.h
+++ lib/mis/ils/local_search.h
@@ -4,21 +4,6 @@
  *
  * The original code from Andrade et. al. was kindly provided by Renato Werneck.
  *
- ******************************************************************************
- * Copyright (C) 2015-2017 Sebastian Lamm <lamm@ira.uka.de>
- *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 2 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 
 #ifndef _DIRECT_LOCAL_H_
