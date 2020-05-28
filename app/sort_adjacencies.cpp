204f00028b3d4d1909b63359a4d54b9d3f117b7e
Christian Schulz <christian.schulz@univie.ac.at>
2020-05-28 17:45:37 +0200

    rewritten adj sorter to support edge and node weights

--- app/sort_adjacencies.cpp
+++ app/sort_adjacencies.cpp
@@ -16,10 +16,18 @@
 
 using namespace std;
 
+struct WeightedEdge {
+        long target;
+        long weight;
+        bool operator < (const WeightedEdge& rhs) const {
+                return target < rhs.target;
+        }
+};
+
 // This program implements the functions to sort the neighborhood of each node of an input graph
 int main(int argn, char **argv) {
         if (argn != 2) {
-            std::cout <<  "Usage: sort_adjancencies FILE"  << std::endl;
+            std::cout <<  "Usage: sort_adjacencies FILE"  << std::endl;
             exit(0);
         }
 
@@ -29,45 +37,80 @@ int main(int argn, char **argv) {
         // open file for reading
         std::ifstream in(filename.c_str());
         if (!in) {
-            std::cerr << "Error opening " << filename << std::endl;
-            return 1;
+                std::cerr << "Error opening " << filename << std::endl;
+                return 1;
         }
 
         std::getline(in,line);
         //skip comments
-        while (line[0] == '%') {
-            std::getline(in, line);
+        while( line[0] == '%' ) {
+                std::getline(in, line);
         }
 
+        int ew = 0;
         long nmbNodes;
         long nmbEdges;
-        long ew = 0;
-
         std::stringstream ss(line);
         ss >> nmbNodes;
         ss >> nmbEdges;
         ss >> ew;
-        if(ew != 0) 
-                std::cout <<  "sorting adjacencies only supported for unweighted graphs."  << std::endl;
 
-        std::cout <<  nmbNodes << " " << nmbEdges << std::endl;
-        while (std::getline(in, line)) {
-            if (line[0] == '%') continue;
+        std::cout <<  nmbNodes << " " << nmbEdges << " " << ew << std::endl;
+        bool read_ew = false;
+        bool read_nw = false;
 
-            std::stringstream ss(line);
-            std::vector<long> adjacent_nodes;
+        if(ew == 1) {
+                read_ew = true;
+        } else if (ew == 11) {
+                read_ew = true;
+                read_nw = true;
+        } else if (ew == 10) {
+                read_nw = true;
+        }
+        
+        while(  std::getline(in, line)) {
+       
+                if (line[0] == '%') { // a comment in the file
+                        continue;
+                }
+
+                std::stringstream ss(line);
+
+                long weight = 1;
+                if( read_nw ) {
+                        ss >> weight;
+                        std::cout <<  weight << " ";
+                }
+
+                long target;
+                std::vector< WeightedEdge > edges_with_weight;
+                while( ss >> target ) {
+                        WeightedEdge e;
+                        e.target = target;
+                        e.weight = 1;
+                        long edge_weight = 1;
+                        if( read_ew ) {
+                                ss >> edge_weight;
+                                e.weight = edge_weight;
+                        }
+                        edges_with_weight.push_back(e);
+                }
+                std::sort(edges_with_weight.begin(), edges_with_weight.end());
+                if(read_ew) {
+                        for( unsigned long i = 0; i < edges_with_weight.size(); i++) {
+                                std::cout <<  edges_with_weight[i].target << " " << edges_with_weight[i].weight << " " ;
+                        }
                 
-            long target;
-            while (ss >> target) {
-                adjacent_nodes.push_back(target);
-            }
-            std::sort(adjacent_nodes.begin(), adjacent_nodes.end());
-            for( unsigned long i = 0; i < adjacent_nodes.size(); i++) {
-                    std::cout <<  adjacent_nodes[i] << " ";
-            }
-            std::cout <<  std::endl;
+                } else {
+                        for( unsigned long i = 0; i < edges_with_weight.size(); i++) {
+                                std::cout <<  edges_with_weight[i].target << " ";
+                        }
+                }
+                std::cout << std::endl;
 
-            if (in.eof()) break;
+                if(in.eof()) {
+                        break;
+                }
         }
 
         return 0;
