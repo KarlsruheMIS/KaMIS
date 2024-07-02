2b2f633bdbeb691557270716f87073fc044f5c51
Christian Schulz <christian.schulz.phone@gmail.com>
2024-07-02 09:46:52 +0200

    fixed memory leak in weighted branch and reduce

--- extern/KaHIP/lib/data_structure/graph_access.h
+++ extern/KaHIP/lib/data_structure/graph_access.h
@@ -435,6 +435,9 @@ inline void graph_access::set_partition_count(PartitionID count) {
 }
 
 inline int graph_access::build_from_metis(int n, int* xadj, int* adjncy) {
+        if(graphref != NULL) {
+                delete graphref;
+        }
         graphref = new basicGraph();
         start_construction(n, xadj[n]);
 
@@ -455,6 +458,9 @@ inline int graph_access::build_from_metis(int n, int* xadj, int* adjncy) {
 }
 
 inline int graph_access::build_from_metis_weighted(int n, int* xadj, int* adjncy, int * vwgt, int* adjwgt) {
+        if(graphref != NULL) {
+                delete graphref;
+        }
         graphref = new basicGraph();
         start_construction(n, xadj[n]);
 
