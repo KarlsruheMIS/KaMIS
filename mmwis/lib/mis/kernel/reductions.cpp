d26cfc3c86cf39c37382041729245bdfa17eabc0
ernestine-grossmann <e.grossmann@stud.uni-heidelberg.de>
2024-10-26 11:15:45 +0200

    speed up single_edge reduction

--- mmwis/lib/mis/kernel/reductions.cpp
+++ mmwis/lib/mis/kernel/reductions.cpp
@@ -1065,9 +1065,9 @@ bool single_edge_reduction::reduce(branch_and_reduce_algorithm* br_alg) {
                     // compute w(N(neighbor)\N(node))
                     partial_neighbor_sum = 0;
                     for (NodeID second_neighbor : status.graph[neighbor]) {
-                        if (status.node_status[second_neighbor] == IS_status::not_set && neighbors.add(second_neighbor)) {
+                        if (status.node_status[second_neighbor] == IS_status::not_set && neighbors.get(second_neighbor)) {
                             partial_neighbor_sum += status.weights[second_neighbor];
-                            neighbors.remove(second_neighbor);
+							if (partial_neighbor_sum >= status.weights[neighbor]) break;
                         }
                     }
                  
